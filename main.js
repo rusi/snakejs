37b62f5b1d49b13a045d2e209076e8b8bdf5a69a
rusi <rusi@me.com>
2014-07-28 22:02:02 +0000

    changed movement

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 var mainState = {
     head: null,
     cursor: null,
+    step: {x: 0, y: 0},
+    snakeSpeed: 200,
     
     preload: function() {
         game.load.image('head', 'assets/player.png');
@@ -11,18 +13,80 @@ var mainState = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.cursor = game.input.keyboard.createCursorKeys();
         
-        this.head = game.add.sprite(200, 200, 'head');
-        this.head.anchor.set(0.5, 0.5);
-        //console.log(this.head.width + ", " + this.head.height);
-        this.head.scale.x = 2;
-        this.head.scale.y = 2;
+        this.head = game.add.sprite(0, 0, 'head');
+        // this.head.anchor.set(0.5, 0.5);
+        // this.head.scale.x = 2;
+        // this.head.scale.y = 2;
         game.physics.arcade.enable(this.head);
         this.head.body.collideWorldBounds = true;
-        //console.log(this.head.width + ", " + this.head.height);
+        
+        var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        var downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        var leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        var rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        
+        upKey.onDown.add(this.goUp, this);
+        downKey.onDown.add(this.goDown, this);
+        leftKey.onDown.add(this.goLeft, this);
+        rightKey.onDown.add(this.goRight, this);
+        
+        game.time.events.loop(this.snakeSpeed, this.moveSnake, this);
+        //this.moveSnake();
+    },
+    
+    goUp: function() {
+        this.step.x = 0;
+        this.step.y = -this.head.height;
+    },
+    goDown: function() {
+        this.step.x = 0;
+        this.step.y = this.head.height;
+    },
+    goLeft: function() {
+        this.step.x = -this.head.width;
+        this.step.y = 0;
+    },
+    goRight: function() {
+        this.step.x = this.head.width;
+        this.step.y = 0;
+    },
+    
+    moveSnake: function() {
+        if (this.tween && this.tween.isRunning)
+            return;
+            
+        var toX = this.head.x + this.step.x;
+        var toY = this.head.y + this.step.y;
+        if (toX < 0 || toY < 0 || toX >= game.world.width || toY >= game.world.height) {
+            this.step.x = 0;
+            this.step.y = 0;
+        } else {
+            // this.head.x = toX;
+            // this.head.y = toY;
+            // this.step.x = 0;
+            // this.step.y = 0;
+            this.tween = game.add.tween(this.head);
+            this.tween.to({x: toX, y: toY}, this.snakeSpeed, null, true);
+            this.tween.onComplete.addOnce(function() {
+        		this.moveSnake();
+        	}, this);
+        }
     },
     
     update: function() {
-        this.movePlayer();
+        //this.movePlayer();
+    },
+    
+    render: function() {
+        //game.debug.spriteInfo(this.head, 32, 32);
+        var s = game.world.width / this.head.width;
+        var w = this.head.width;
+        for (var i = 0; i < s; ++i) {
+            game.debug.geom(new Phaser.Line(w*i, 0, w*i, game.world.height));
+        }
+        for (var i = 0; i < s; ++i) {
+            game.debug.geom(new Phaser.Line(0, w*i, game.world.width, w*i));
+        }
     },
     
     movePlayer: function() {
